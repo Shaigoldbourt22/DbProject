ce86c33850922286fff558cb46c2ae74f3d1b26f
shai <t-sgoldbourt@microsoft.com>
2025-01-11 13:24:22 +0200

    Add genre fetching and improve movie data insertion

--- src/api_data_retrieve.py
+++ src/api_data_retrieve.py
@@ -2,10 +2,15 @@ import requests
 import mysql.connector
 import time
 
-API_KEY = 'your_tmdb_api_key'
+API_KEY = '733e89cf8650182d336cd0a0d5ad71d0'
 
 def fetch_movies(page):
     url = f"https://api.themoviedb.org/3/movie/popular?api_key={API_KEY}&page={page}"
+    response = requests.get(url)
+    return response.json()
+
+def fetch_genres():
+    url = f"https://api.themoviedb.org/3/genre/movie/list?api_key={API_KEY}"
     response = requests.get(url)
     return response.json()
 
@@ -18,18 +23,23 @@ def insert_data():
     )
     cursor = connection.cursor()
     
+    genres = fetch_genres()
+    genre_dict = {genre['id']: genre['name'] for genre in genres['genres']}
+    
     for page in range(1, 251):  # Fetching 5000 movies (20 movies per page)
         movie_data = fetch_movies(page)
         for movie in movie_data['results']:
+            genre_names = ', '.join([genre_dict[genre_id] for genre_id in movie['genre_ids']])
             cursor.execute("""
             INSERT INTO Movies (title, release_year, genre)
             VALUES (%s, %s, %s)
-            """, (movie['title'], movie['release_date'][:4], ', '.join([genre['name'] for genre in movie['genre_ids']])))
+            """, (movie['title'], movie['release_date'][:4], genre_names))
             
             # Assuming you have a way to fetch actor data and insert into Actors and MovieActors tables
             
         # Sleep to avoid hitting the rate limit
         time.sleep(1)
+        print(f"Inserted movies from page {page}")
     
     connection.commit()
     cursor.close()
